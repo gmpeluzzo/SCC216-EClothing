294b0c6a5e9ffd11cacdb3ed6e376bdcb8540a74
Bruno Pauluci Reinato <bruno.reinato@raccoon.ag>
2021-11-17 22:31:00 -0300

    fundo transparente e css alterado pra ficar igual o milestone 1

--- eclothing/src/components/LoginContainer/loginForm.js
+++ eclothing/src/components/LoginContainer/loginForm.js
@@ -3,45 +3,49 @@ import background from "./watermark.jpg"
 import styled from "styled-components"
 
 const Form = styled.form`
-    justify-content: center;
-    align-items: center;
-    width: 450px;
+    opacity: 1;
+    position: absolute;
+    z-index: 2;
+    margin: 0 0 50px 0;
 `;
 
 const Wrapper = styled.div`
-    background-image:url(${background});
-    background-position: center;
-    background-repeat: no-repeat;
-    height: 800px;
-    background-size: 800px 800px;
+    background-color: white;
+    height: calc(100vh - 85px);
+    width: 100%;
     display: flex;
+    position: relative;
     justify-content: center;
     align-items: center;
+    margin: 0;
+    padding: 0;
 `;
 
+const Image = styled.img`
+    position: absolute;
+    z-index: 1;
+    width: 800px;
+    height: 775px;
+    opacity: 0.27;
+`
+
 const TextField = styled.input`
-    width: 100%;
-    padding: 17px;
-    border: 1px solid #ccc;
-    border-radius: 0px;
-    box-sizing: border-box;
+    border: 1px solid #8492A6;
+    width: 452px;
+    height: 50px;
 `;
 
 const Button = styled.input`
-    background-color: #13CE66;
-    color: white;
-    padding: 12px;
-    width: 100%;
+    width: 456px;
     border: none;
-    border-radius: 0px;
-    cursor: pointer;
-    float: center;
-    font-size: 40px;
+    background-color: #13CE66;
+    font-size: 39px;
+    color: white;
+    height: 70px;
 `;
 
 const Label = styled.h2`
     font-weight: normal;
-    line-height: 1px;
 `;
 
 const LoginForm = () => {
@@ -49,12 +53,13 @@ const LoginForm = () => {
     return (
         <Wrapper className="formWrapper">
             <Form>
-                <Label>Login</Label><br/>
-                <TextField type="text" className="login"/><br/><br/>
-                <Label>Password</Label><br/>
+                <Label>Login</Label>
+                <TextField type="text" className="login"/><br/>
+                <Label>Password</Label>
                 <TextField type="password" className="password"/> <br/><br/><br/>
                 <Button type="submit" value="Sign in" class="button"/>
             </Form>
+            <Image src={background}/>
         </Wrapper>
     )
 }
